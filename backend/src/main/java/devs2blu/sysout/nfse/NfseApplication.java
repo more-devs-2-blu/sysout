13df0673646f1b296000d68ce622e4421dd2429b
eduardofloriani <eeduardofloriani@gmail.com>
2023-03-10 21:37:59 -0300

    Initial commit

---
+++ backend/src/main/java/devs2blu/sysout/nfse/NfseApplication.java
@@ -0,0 +1,13 @@
+package devs2blu.sysout.nfse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/devs2blu/sysout/nfse/NfseApplicationTests.java
@@ -0,0 +1,13 @@
+package devs2blu.sysout.nfse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
