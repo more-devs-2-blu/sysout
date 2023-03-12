9448f7715e93905b1ead49a2555764070f3afc47
eduardofloriani <eeduardofloriani@gmail.com>
2023-03-12 17:13:35 -0300

    extract user information from jwt token

---
+++ backend/src/main/java/devs2blu/sysout/nfse/exceptions/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package devs2blu.sysout.nfse.exceptions;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+}
